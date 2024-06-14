1c2e65ea87f2aede0955ce16bc64e4318d826276
Haruki1090 <haruharu.com.109607@gmail.com>
2024-06-15 01:04:00 +0900

    ⚡️ ２重props解消にあたる改善

--- pages/about.js
+++ pages/about.js
@@ -17,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <Headline title="About Page" pagePath="about" />
+        <Headline  pagePath="about"/>
 
         <div className={styles.center}>
           <Image
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <Headline title="Index Page" pagePath="index" />
+        <Headline pagePath="index" />
 
         <div className={styles.center}>
           <Image
