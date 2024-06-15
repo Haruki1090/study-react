e299119ecb59e9ff8274bdbaf6569f061825d1fd
Haruki1090 <haruharu.com.109607@gmail.com>
2024-06-15 15:24:05 +0900

    ♻️ Refactor Headline component to use props for code editing

--- components/Headline.js
+++ components/Headline.js
@@ -5,14 +5,12 @@ import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export function Headline(props) {
+  console.log(props);
   return (
     <>
       <div className={styles.description}>
         <h1 className={styles.title}>{props.pagePath} page</h1>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>pages/{props.pagePath}.js</code>
-        </p>
+        <p>Get started by editing&nbsp;{props.code}</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
