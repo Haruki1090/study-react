ddcfce02160f0f5e4b6b59a6f1f1623f66c13846
Haruki1090 <haruharu.com.109607@gmail.com>
2024-06-28 17:38:22 +0900

    ✨ Mainコンポーネントがマウントされた時に実行されるuseEffect

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,10 +2,23 @@ import { Inter } from "next/font/google";
 import styles from "src/components/Main/Main.module.css";
 import { Links } from "src/components/Links";
 import { Headline } from "src/components/Headline";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export function Main(props) {
+
+  // 各ページのMainコンポーネントがマウントされた時に実行される
+  useEffect(()=>{
+    // マウント時の処理
+    document.body.style.backgroundColor = "lightblue";
+
+    // アンマウント時の処理
+    return ()=>{
+      document.body.style.backgroundColor = "";
+    }
+  },[])
+
   return (
     <>
       <main className={`${styles.main} ${inter.className}`}>
