37f7470b01559e71cbeb0e54fccc91a42137fc46
Haruki1090 <haruharu.com.109607@gmail.com>
2024-06-28 17:14:04 +0900

    ✨ onClickメソッド

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,16 +2,33 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  const handleClick = useCallback((e) => {
+    e.preventDefault();
+    alert("ボタンがクリックされました");
+  }, []);
+
+  // useCallbackを使うと、関数の再生成を抑えることができる
+
   return (
     <>
       <Head>
         <title>Index page</title>
       </Head>
       <Header />
+
+      <a
+        href="/about"
+        onClick={handleClick}
+      >
+        ボタンです
+      </a>
+
       <Main pagePath="index" />
     </>
   );
