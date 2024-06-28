6ffc06c116b3d015d4b9406bfd9e4efca9f2ae1b
Haruki1090 <haruharu.com.109607@gmail.com>
2024-06-28 17:32:34 +0900

    ✨ useEffectを利用したカラー変更

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,19 +2,31 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
-  const handleClick = useCallback((e) => {
-    e.preventDefault();
-    alert("ボタンがクリックされました");
-  }, []);
+
+//   const handleClick = useCallback((e) => {
+//     e.preventDefault();
+//     alert("ボタンがクリックされました");
+//   }, []);
 
   // useCallbackを使うと、関数の再生成を抑えることができる
+
+
+  useEffect(()=>{
+    // マウント時の処理
+    document.body.style.backgroundColor = "lightblue";
+
+    // アンマウント時の処理
+    return ()=>{
+      document.body.style.backgroundColor = "";
+    }
+  },[])
 
   return (
     <>
@@ -23,11 +35,12 @@ export default function Home() {
       </Head>
       <Header />
 
-      <Link href="/about" onClick={handleClick}>
+      {/* <Link href="/about" onClick={handleClick}>
         ボタンです
-      </Link>
+      </Link> */}
 
       <Main pagePath="index" />
     </>
   );
 }
+
