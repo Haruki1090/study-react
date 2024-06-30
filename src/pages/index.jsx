c4bf6aa6317d64921737e50df9365322c3be20fe
Haruki1090 <haruharu.com.109607@gmail.com>
2024-06-30 15:33:25 +0900

    ✨ useStateで配列を扱う

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,10 +8,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
-
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-  };
+  const [array, setArray] = useState([]);
 
   useEffect(() => {
     // マウント時の処理
@@ -33,10 +30,19 @@ export default function Home() {
       return;
     }
     setText(e.target.value)
-  }, [])
+  }, []);
 
   const handleDisplay = useCallback(() => { setIsShow((prevIsShow) => !prevIsShow) }, []);
 
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素が既に存在します");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
 
   return (
     <>
@@ -47,11 +53,19 @@ export default function Home() {
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタンです</button>
       <button onClick={handleDisplay} >{isShow ? "非表示" : "表示"}</button>
+      <button onClick={handleAdd}>追加</button>
       <input
         type="text"
         value={text}
         onChange={handleInputChange}
       />
+      <ul>
+        {array.map((item) => {
+          return (
+            <li key={item}>{item}</li>
+          )
+        })}
+      </ul>
 
       <Main pagePath="index" />
     </>
